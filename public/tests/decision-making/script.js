43137fa34e34d7cadcffd7ff3df9e1337266980b
Lorenzo Paredes <lsparedes@ing.ucsc.cl>
2024-08-03 00:01:59 -0400

    bloques intermedios, botón siguiente y variables de estimulos

--- public/tests/decision-making/script.js
+++ public/tests/decision-making/script.js
@@ -1,60 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let currentTrial = 0;
-    const maxPracticeTrials = 10;
-    const maxTestTrials = 60;
-    let trials = [];
-    let startTime;
-    let results = [];
-    let practiceMode = true;
-    let selectionMade = false;
-    let score = 0;
-  
-    const trialIndicator = document.getElementById('trialIndicator');
-    const practiceTrial = document.getElementById('practiceTrial');
-    const feedbackScreen = document.getElementById('feedbackScreen');
-    const feedbackImage1 = document.getElementById('feedbackImage1');
-    const feedbackImage2 = document.getElementById('feedbackImage2');
-    const feedbackMessage = document.getElementById('feedbackMessage');
-    const blockFinishScreen = document.getElementById('blockFinishScreen');
-    const endScreen = document.getElementById('endScreen');
-    const leftSlot = document.getElementById('leftSlot');
-    const rightSlot = document.getElementById('rightSlot');
-    const instructions = document.getElementById('instructions');
-    const scoreAmount = document.getElementById('scoreAmount');
-  
-    document.getElementById('startPracticeButton').addEventListener('click', () => {
+  let currentTrial = 0;
+  const cantidad_ensayos_prueba = 10;
+  const cantidad_ensayos_bloque_1 = 20;
+  const cantidad_ensayos_bloque_2 = 20;
+  const cantidad_ensayos_bloque_3 = 20;
+  let trials = [];
+  let startTime;
+  let results = [];
+  let practiceMode = true;
+  let selectionMade = false;
+  let score = 0;
+  let currentBlock = 0; // Track the current block
+
+  const trialIndicator = document.getElementById('trialIndicator');
+  const practiceTrial = document.getElementById('practiceTrial');
+  const feedbackScreen = document.getElementById('feedbackScreen');
+  const feedbackImage1 = document.getElementById('feedbackImage1');
+  const feedbackImage2 = document.getElementById('feedbackImage2');
+  const feedbackMessage = document.getElementById('feedbackMessage');
+  const blockFinishScreen = document.getElementById('blockFinishScreen');
+  const blockFinishMessage = document.getElementById('blockFinishMessage');
+  const endScreen = document.getElementById('endScreen');
+  const leftSlot = document.getElementById('leftSlot');
+  const rightSlot = document.getElementById('rightSlot');
+  const instructions = document.getElementById('instructions');
+  const scoreAmount = document.getElementById('scoreAmount');
+
+  document.getElementById('startPracticeButton').addEventListener('click', () => {
       startPractice();
-    });
-  
-    document.getElementById('fullscreenButton').addEventListener('click', () => {
+  });
+
+  document.getElementById('fullscreenButton').addEventListener('click', () => {
       if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
+          document.documentElement.requestFullscreen();
       }
-    });
-  
-    leftSlot.addEventListener('click', () => {
+  });
+
+  leftSlot.addEventListener('click', () => {
       if (!selectionMade) {
-        selectionMade = true;
-        selectMachine('left');
+          selectionMade = true;
+          selectMachine('left');
       }
-    });
-  
-    rightSlot.addEventListener('click', () => {
+  });
+
+  rightSlot.addEventListener('click', () => {
       if (!selectionMade) {
-        selectionMade = true;
-        selectMachine('right');
+          selectionMade = true;
+          selectMachine('right');
       }
-    });
-  
-    document.getElementById('startNextBlockButton').addEventListener('click', () => {
+  });
+
+  document.getElementById('startNextBlockButton').addEventListener('click', () => {
+      currentBlock++;
       startTestBlock();
-    });
-  
-    document.getElementById('downloadResultsButton').addEventListener('click', () => {
+  });
+
+  document.getElementById('downloadResultsButton').addEventListener('click', () => {
       downloadResults();
-    });
-  
-    function startPractice() {
+  });
+
+  function startPractice() {
       practiceMode = true;
       currentTrial = 0;
       trials = generatePracticeTrials();
@@ -62,19 +67,19 @@ document.addEventListener('DOMContentLoaded', () => {
       instructions.style.display = 'none';
       practiceTrial.style.display = 'block';
       showNextTrial();
-    }
-  
-    function startTestBlock() {
+  }
+
+  function startTestBlock() {
       practiceMode = false;
       currentTrial = 0;
-      trials = generateTestTrials();
+      trials = generateTestTrials(currentBlock);
       startTime = Date.now();
       blockFinishScreen.style.display = 'none';
       practiceTrial.style.display = 'block';
       showNextTrial();
-    }
-  
-    function showNextTrial() {
+  }
+
+  function showNextTrial() {
       selectionMade = false;
       const trial = trials[currentTrial];
       trialIndicator.innerText = practiceMode ? `P${currentTrial + 1}` : `E${currentTrial + 1}`;
@@ -83,69 +88,100 @@ document.addEventListener('DOMContentLoaded', () => {
       endScreen.style.display = 'none';
       resetSlotMachines();
       practiceTrial.style.display = 'block';
-    }
-  
-    function finishBlock() {
+  }
+
+  function finishBlock() {
       practiceTrial.style.display = 'none';
       feedbackScreen.style.display = 'none';
       if (practiceMode) {
-        blockFinishScreen.style.display = 'block';
+          blockFinishMessage.innerText = "Práctica completada";
+          blockFinishScreen.style.display = 'block';
+          document.getElementById('startNextBlockButton').style.display = 'block';
       } else {
-        endScreen.style.display = 'block';
+          switch (currentBlock) {
+              case 0:
+                  blockFinishMessage.innerText = "Bloque de práctica completado";
+                  blockFinishScreen.style.display = 'block';
+                  break;
+              case 1:
+                  blockFinishMessage.innerText = "Bloque 1 completado";
+                  blockFinishScreen.style.display = 'block';
+                  break;
+              case 2:
+                  blockFinishMessage.innerText = "Bloque 2 completado";
+                  blockFinishScreen.style.display = 'block';
+                  break;
+              case 3:
+                  endScreen.style.display = 'block';
+                  break;
+          }
       }
-    }
-  
-    function generatePracticeTrials() {
+  }
+
+  function generatePracticeTrials() {
       const practiceTrials = [];
-      for (let i = 0; i < maxPracticeTrials; i++) {
-        practiceTrials.push({
-          leftReward: Math.random() < 0.5,
-          rightReward: Math.random() < 0.5
-        });
+      for (let i = 0; i < cantidad_ensayos_prueba; i++) {
+          practiceTrials.push({
+              leftReward: Math.random() < 0.5,
+              rightReward: Math.random() < 0.5
+          });
       }
       return practiceTrials;
-    }
-  
-    function generateTestTrials() {
+  }
+
+  function generateTestTrials(block) {
       const testTrials = [];
-      for (let i = 0; i < maxTestTrials; i++) {
-        let leftReward, rightReward;
-        if (i < 20) {
-          leftReward = Math.random() < 0.25;
-          rightReward = Math.random() < 0.75;
-        } else if (i < 40) {
-          leftReward = Math.random() < 0.75;
-          rightReward = Math.random() < 0.25;
-        } else {
-          leftReward = Math.random() < 0.25;
-          rightReward = Math.random() < 0.75;
-        }
-        testTrials.push({
-          leftReward,
-          rightReward
-        });
+      let blockTrialsCount;
+      let leftRewardChance, rightRewardChance;
+
+      switch (block) {
+          case 1:
+              blockTrialsCount = cantidad_ensayos_bloque_1;
+              leftRewardChance = 0.25;
+              rightRewardChance = 0.75;
+              break;
+          case 2:
+              blockTrialsCount = cantidad_ensayos_bloque_2;
+              leftRewardChance = 0.75;
+              rightRewardChance = 0.25;
+              break;
+          case 3:
+              blockTrialsCount = cantidad_ensayos_bloque_3;
+              leftRewardChance = 0.25;
+              rightRewardChance = 0.75;
+              break;
+          default:
+              return [];
       }
+
+      for (let i = 0; i < blockTrialsCount; i++) {
+          testTrials.push({
+              leftReward: Math.random() < leftRewardChance,
+              rightReward: Math.random() < rightRewardChance
+          });
+      }
+
       return testTrials;
-    }
-  
-    function resetSlotMachines() {
+  }
+
+  function resetSlotMachines() {
       leftSlot.src = 'img/slot-machine-left-up.png';
       rightSlot.src = 'img/slot-machine-right-up.png';
-    }
-  
-    function selectMachine(side) {
+  }
+
+  function selectMachine(side) {
       if (side === 'left') {
-        leftSlot.src = 'img/slot-machine-left-down.png';
+          leftSlot.src = 'img/slot-machine-left-down.png';
       } else {
-        rightSlot.src = 'img/slot-machine-right-down.png';
+          rightSlot.src = 'img/slot-machine-right-down.png';
       }
-  
+
       setTimeout(() => {
-        showFeedback(side);
+          showFeedback(side);
       }, 1000);
-    }
-  
-    function showFeedback(side) {
+  }
+
+  function showFeedback(side) {
       const trial = trials[currentTrial];
       const reward = trial[`${side}Reward`];
       feedbackImage1.src = reward ? 'img/win.jpg' : 'img/lose.jpg';
@@ -158,17 +194,17 @@ document.addEventListener('DOMContentLoaded', () => {
       practiceTrial.style.display = 'none';
       feedbackScreen.style.display = 'block';
       currentTrial++;
-  
+
       setTimeout(() => {
-        if (currentTrial < trials.length) {
-          showNextTrial();
-        } else {
-          finishBlock();
-        }
+          if (currentTrial < trials.length) {
+              showNextTrial();
+          } else {
+              finishBlock();
+          }
       }, 3000);
-    }
-  
-    function downloadResults() {
+  }
+
+  function downloadResults() {
       let csvContent = "data:text/csv;charset=utf-8,";
       csvContent += "item,respuesta seleccionada (rojo o azul),seguridad (valor seleccionado en la barra),evaluacion (correcta o incorrecta),tiempo\n";
       csvContent += results.map(e => e.join(",")).join("\n");
@@ -177,6 +213,5 @@ document.addEventListener('DOMContentLoaded', () => {
       link.setAttribute("download", "results.csv");
       document.body.appendChild(link);
       link.click();
-    }
-  });
-  
+  }
+});
