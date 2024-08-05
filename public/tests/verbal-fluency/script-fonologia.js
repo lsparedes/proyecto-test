ebcb1f39990234bf4e81aa8aa245dbb5d2904cff
José Rivas <41880508+JoseRivasE@users.noreply.github.com>
2024-08-05 17:50:43 -0400

    test 4 categoria

--- public/tests/verbal-fluency/script-fonologia.js
+++ public/tests/verbal-fluency/script-fonologia.js
@@ -142,6 +142,7 @@ function loadAudio(part) {
         letterDisplay.style.display = 'none';
         document.getElementById('startRecButton' + part).style.display = 'inline-block'; // Mostrar botón para comenzar grabación
         document.getElementById('nextButton' + part).style.display = 'inline-block'; // Mostrar la flecha
+        document.getElementById('startRecButton' + part).click(); // Iniciar grabación automáticamente
     });
 }
 
@@ -181,7 +182,7 @@ function downloadRecordingAndTime() {
     // link.click();
 
     const zip = new JSZip();
-    zip.file("tiempo_total.txt", timeBlob);
+    zip.file("tiempo_total_mano_utilizada.txt", timeBlob);
 
     const audio1Blob = new Blob(audioChunks[1], { type: 'audio/wav' });
     zip.file("Fonológica - Parte 1.wav", audio1Blob);
@@ -198,10 +199,10 @@ function downloadRecordingAndTime() {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
-        const formattedDate = `${year}${month}${day}`;
+        const formattedDate = `${day}_${month}_${year}`;
         
         // Construir el nombre del archivo ZIP
-        const fileName = `${participantID}_TEST_4_VERBAL_FLUENCY_FONOLOGIA_${formattedDate}.zip`;
+        const fileName = `${participantID}_verbal_fluency_fonologia_${formattedDate}.zip`;
 
         zipLink.download = fileName;
         zipLink.click();
