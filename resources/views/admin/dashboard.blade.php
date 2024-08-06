9f1ed182dff665c293ecb49965b8aac9660d7bb2
Lorenzo Paredes <lsparedes@ing.ucsc.cl>
2024-08-06 03:28:35 -0400

    cambios login en catálogo y omisión de funcionalidades que ya no se usarán

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
             <li class="breadcrumb-item active">Dashboard</li>
         </ol>
         <div class="row">
-            <div class="col-xl-3 col-md-6">
+            <!-- <div class="col-xl-3 col-md-6">
                 <div class="card bg-primary text-white mb-4">
                     <div class="card-body">Card</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
@@ -27,7 +27,7 @@
                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
         </div>
     </div>
