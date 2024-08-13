751be068faa37def8277788ecc09a7d30db0dbe1
Nicoperez19 <nperez@ing.ucsc.cl>
2024-08-12 22:55:26 -0400

    cambios en detalles vienna

--- public/tests/letters/app.js
+++ public/tests/letters/app.js
@@ -688,7 +688,7 @@ function validateClicks() {
     csvContent += `Clics Derecha;${rightClicks}\n`;
     csvContent += `Total Clics;${clicks.length}\n`;
     csvContent += `Search Speed (segundos);${searchSpeed}\n`;
-    csvContent += `Search Distance;${searchDistanceFormatted}\n`;
+    csvContent += `Search Distance;${searchDistanceFormatted} px\n`;
     csvContent += `Centro de Cancelacion en Pixeles (X, Y);(${centerX.toFixed(2)}, ${centerY.toFixed(2)})\n`;
     csvContent += `Centro de Cancelacion Normalizado (X, Y);(${normalizedCenterX.toFixed(2)}, ${normalizedCenterY.toFixed(2)})\n`;
     csvContent += `CoC; ${Math.abs(normalizedCenterX.toFixed(2))}\n`;
